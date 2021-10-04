eebeaa2d22d518f5047e3dfe496fc0a909d97688
markshepherd <mark.e.shepherd@gmail.com>
2021-10-04 16:30:20 -0700

    work in progress

--- Product/MySerial.h
+++ Product/MySerial.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#undef SERIAL_ENABLED
+#define SERIAL_ENABLED
 // It's probably safe to #define SERIAL_ENABLED in the product build,
 // but it's unlikely to be used, and maybe could cause trouble somehow,
 // so I think it's better to #undef it in the product.
