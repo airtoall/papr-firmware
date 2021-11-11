1868ba31c3311e8973c5f6c388a583118007a559
markshepherd <mark.e.shepherd@gmail.com>
2021-11-10 21:35:13 -0800

    eliminate unused code.

--- Product/MySerial.h
+++ Product/MySerial.h
@@ -4,7 +4,6 @@
 
 void serialBegin(bool inputAllowed);
 void serialEnd();
-bool serialActive();
 void serialPrintf(const char* __fmt, ...);
 void serialPrint(const __FlashStringHelper* string);
 void serialPrintln(const __FlashStringHelper* string);
